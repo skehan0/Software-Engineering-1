1d4ec5d0ae2b71cd40d9ff4038b6ea8330224fb7
skehan0 <g.skehan1@nuigalway.ie>
2023-03-23 16:52:43 +0000

    blog

--- vue-project/src/pages/Blog.vue
+++ vue-project/src/pages/Blog.vue
@@ -1,49 +1,146 @@
 <template>
-  <div class="container-fluid">
-    <h1 class="blog-title">Welcome to my blog page</h1>
-    <p class="blog-text">This is my blog page for my Spice app</p>
+  <h1>Welcome to my new Blog page</h1>
+  <p>This is my very first blog entry</p>
+  <div class="container mt-5">
     <div class="mb-3">
       <label for="exampleFormControlInput1" class="form-label">Email address</label>
-      <input type="email"  v-model= "handle" class="form-control" id="exampleFormControlInput1"
-             placeholder="name@example.com">
+      <input type="email" class="form-control" v-model="handle" id="exampleFormControlInput1" placeholder="name@example.com">
     </div>
     <div class="mb-3">
       <label for="exampleFormControlTextarea1" class="form-label">Comment</label>
-      <textarea class="form-control"  v-model="comment" id="exampleFormControlTextarea1" rows="3"></textarea>
+      <textarea class="form-control" v-model="comment" id="exampleFormControlTextarea1" rows="3"></textarea>
     </div>
     <div class="mb-3 right">
       <button type="button" @click="postComment" class="btn btn-primary">Post Comment</button>
     </div>
+    <div class="mb-3 right">
+      <button type="button" @click="getComments" class="btn btn-primary">Show Comments</button>
+    </div>
+    <!-- Checks to make sure there are actual comments to display -->
+    <div v-if="commentsArray.length > 0">
+      <ul>
+      <li v-for="comment in commentsArray">
+        <div v-if="!editing">
+          <span @click="enableEditing(comment.data.comment)">
+            {{comment.data.comment}}</span>
+        </div>
+        <div v-if="editing">
+          <input v-model="tempValue" class="input"/>
+          <button @click="disableEditing"> Cancel </button>
+          <button @click="save(comment.id)"> Save </button>
+        </div>
+        <div v-if="this.user">
+           <button type="button" v-if="comment.data.uid == this.user.uid" @click="deleteComment(comment.id)" class="btn btn-primary">Delete Comment</button>
+        </div>
+      </li>
+      </ul>
+    </div>
   </div>
 </template>
-
 <script>
 import app from '../api/firebase';
 import { getFunctions, httpsCallable, connectFunctionsEmulator } from "firebase/functions";
+import {getAuth, onAuthStateChanged} from "firebase/auth";
+
 export default {
-  name: "Blog",
   data() {
     return {
       handle: '',
-      comment: ''
+      comment: '',
+      commentsArray:[],
+      editing:false,
+      tempValue:null,
+      user:null
     }
   },
-  methods : {
-    postComment () {
-      console.log(this.handle);
-      console.log(this.comment);
+  created(){
+    // Check for logged in user
+    const auth = getAuth(app);
+    onAuthStateChanged(auth, (user) => {
 
+      if (user) {
+        console.log("User", user);
+        this.user = user;
+        this.handle = user.email;
+        // User is signed in
+      } else {
+        console.log("No user found")
+        // User is not signed in
+      }
+    });
+    this.getComments();
+    //window.setInterval(this.getComments, 1000);
+  },
+  methods : {
+    postComment() {
+      let loader = this.$loading.show({    // Optional parameters
+        loader: 'dots',
+        container: this.$refs.container,
+        canCancel: false
+      });
       const functions = getFunctions(app);
-      if(window.location.hostname === 'localhost') // Check if working locally
-      connectFunctionsEmulator(functions, "localhost", 5001);
-      const postComment = httpsCallable(functions, 'postcomment');
-      postComment({"handle": this.handle, "comment":
-        this.comment})
-          .then((result) => {
-// Read result of the Cloud Function.
-            /** @type {any} */
-            console.log(result);
-          });
+      // Uncomment this code if your local emulators are running and you wish to test locally
+      //if(window.location.hostname === 'localhost') // Checks if working locally
+        //connectFunctionsEmulator(functions, "localhost", 5001);
+      const postComment = httpsCallable(functions, 'postusercomment');
+      postComment({"handle": this.handle, "comment": this.comment}).then((result) => {
+        // Read result of the Cloud Function.
+        // /** @type {any} */
+        loader.hide();
+        this.getComments();
+      });
+    },
+    getComments() {
+      let loader = this.$loading.show({    // Optional parameters
+        loader: 'dots',
+        container: this.$refs.container,
+        canCancel: false
+      });
+      const functions = getFunctions(app);
+      // Uncomment this section if your local emulators are running and you wish to test locally
+      //if(window.location.hostname === 'localhost') // Checks if working locally
+      //connectFunctionsEmulator(functions, "localhost", 5001);
+      const getComments = httpsCallable(functions, 'getcomments');
+      getComments().then((result) => {
+        // Read result of the Cloud Function.
+        // /** @type {any} */
+        console.log(result.data);
+        loader.hide();
+        this.commentsArray = result.data;
+      });
+    },
+    deleteComment(id){
+      const functions = getFunctions(app);
+      // Uncomment this section if your local emulators are running and you wish to test locally
+      //if(window.location.hostname === 'localhost') // Checks if working locally
+      //connectFunctionsEmulator(functions, "localhost", 5001);
+      const deleteComment = httpsCallable(functions, 'deleteusercomment');
+      deleteComment({id:id}).then((result) => {
+        console.log(result.data);
+        if(result.data == "Document successfully deleted")
+          this.getComments();
+      }); // To refresh the client
+    },
+    enableEditing(comment){
+      this.tempValue = comment;
+      this.editing = true;
+      },
+    disableEditing(){
+      this.tempValue = null;
+      this.editing = false;
+    },
+    save(id) {
+      const functions = getFunctions(app);
+      // Uncomment this section if your local emulators are running and you wish to test locally
+      //if(window.location.hostname === 'localhost') // Checks if working locally
+      //connectFunctionsEmulator(functions, "localhost", 5001);
+      const updateComment = httpsCallable(functions, 'updatecomment?id=' + id);
+      // Data field automatically populated by Firebase client lib
+      // JSON that will arrive at the server will be { data : {comment : "text from input"} }
+      updateComment({ comment : this.tempValue }).then((result) => {
+        this.getComments();
+        this.editing = false;
+      });
     }
   }
 }
