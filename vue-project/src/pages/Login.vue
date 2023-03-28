a9d5c572e89298e219fd99cec6897e81b42a1a17
crashkila101 <concoolmccarthy@gmail.com>
2023-03-28 17:18:25 +0100

    fixed up some shtuff

--- vue-project/src/pages/Login.vue
+++ vue-project/src/pages/Login.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="form-wrap">
-    <h1>Login to Spice</h1>
-    <form @submit.prevent="login">
-      <label for="username">Username</label>
-      <input type="text" v-model="username" id="username" required>
-      <label for="password">Password</label>
-      <input type="password" v-model="password" id="password" required>
-      <label for="location">Location</label>
-      <select v-model="location" id="location">
-        <option disabled value="">Please select a location</option>
-        <option v-for="county in counties" :value="county">{{ county }}</option>
-      </select>
+    <form class="login" @submit.prevent="login">
+      <h3>Login to Spice</h3>
+      <div class="inputs">
+        <div class="input">
+          <input type="text" placeholder="email" v-model="username" id="username" required>
+        </div>
+        <div class="input">
+          <input type="password" placeholder="password" v-model="password" id="password" required>
+        </div>
+        <div class="input">
+          <select v-model="location" id="location">
+            <option disabled value="">Please select a location</option>
+            <option v-for="county in counties" :value="county">{{ county }}</option>
+          </select>
+        </div>
+      </div>
       <div class="remember-me">
         <label for="remember">
           <input type="checkbox" v-model="remember" id="remember">
@@ -18,9 +23,11 @@
         </label>
       </div>
       <button type="submit">Login</button>
-      <a class="register" href="Register">Don't have an account? Register</a>
+      <p class="login-register">
+      <router-link class="register" to="/register">Don't have an account? Register</router-link>
+      </p>
+      <div class="angle"></div>
     </form>
-    <div class="angle"></div>
     <div class="background"></div>
   </div>
 </template>
@@ -53,7 +60,7 @@ export default {
           // Signed in
           let user = userCredential.user;
           console.log(user);
-          this.$router.push({path: '/secure'});
+          this.$router.push({ path: '/secure' });
         })
         .catch((error) => {
           let errorCode = error.code;
@@ -66,55 +73,131 @@ export default {
 }
 </script>
 
-  <style lang="scss">
-  .form-wrap {
+<style lang="scss">
+.form-wrap {
+  overflow: hidden;
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
   height: 100vh;
-  color: black;
-  margin-top: 80px;
+  justify-content: center;
+  align-self: center;
+  margin: 0 auto;
+  width: 90%;
 
-  h1 {
-    margin-bottom: 2rem;
+  @media (min-width: 900px) {
+    width: 100%;
+  }
+
+  .login-register {
+    margin-bottom: 32px;
+
+    .register {
+      color: #000;
+    }
   }
 
   form {
+    padding: 0 10px;
+    position: relative;
     display: flex;
     flex-direction: column;
+    justify-content: center;
     align-items: center;
-    width: 20rem;
-    padding: 2rem;
-    background-color: white;
-    border-radius: 0.5rem;
+    flex: 1;
 
-    label {
-      display: block;
-      margin-bottom: 0.5rem;
+    @media (min-width: 900px) {
+      padding: 0 50px;
     }
 
-    input,
-    select {
-      padding: 0.5rem;
-      margin-bottom: 1rem;
-      border-radius: 0.25rem;
-      border: none;
-      box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.1);
+    h3 {
+      text-align: center;
+      color: #303030;
+      padding-bottom: 10px;
+
+      @media (min-width: 900px) {
+        font-size: 40px;
+      }
     }
 
-    .remember-me {
-      display: flex;
-      align-items: center;
-      margin-bottom: 1rem;
+    .inputs {
+      width: 100%;
+      max-width: 350px;
+
+      .input {
+        position: relative;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 25px;
+
+
+        input, select {
+          width: 100%;
+          border: none;
+          background-color: #f2f7f6;
+          padding: 4px 4px 4px 30px;
+          height: 50px;
+          border-radius: 0.5rem;
+
+          &:focus {
+            outline: none;
+          }
+        }
+
+        .icon {
+          width: 12px;
+          position: absolute;
+          left: 6px;
+        }
+      }
     }
 
-    button {
+    .forgot-password {
+      text-decoration: none;
+      color: #000;
+      cursor: pointer;
+      font-size: 14px;
+      margin: 16px 0 32px;
+      border-bottom: 1px solid transparent;
+      transition: 0.5s ease all;
+
+      &:hover {
+        border-color: #303030;
+      }
+    }
+
+    .angle {
+      display: none;
+      position: absolute;
+      background-color: #fff;
+      transform: rotateZ(3deg);
+      width: 60px;
+      right: -30px;
+      height: 101%;
+
+      @media (min-width: 900px) {
+        display: initial;
+      }
+    }
+  }
+
+  .background {
+    display: none;
+    flex: 2;
+    background-size: cover;
+    background-image: url("../assets/background.png");
+    width: 100%;
+    height: 100%;
+
+    @media (min-width: 900px) {
+      display: initial;
+    }
+  }
+      button {
       padding: 0.5rem 1rem;
-      background-color: #007bff;
+      background-color: black;
       color: #fff;
       border: none;
-      border-radius: 0.25rem;
+      border-radius: 1rem;
       box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.1);
       cursor: pointer;
       transition: background-color 0.2s ease-in-out;
@@ -123,39 +206,5 @@ export default {
         background-color: #0062cc;
       }
     }
-
-    select {
-      width: 100%;
-    }
-  }
-
-  .angle {
-    display: none;
-    position: absolute;
-    background-color: #fff;
-    transform: rotateZ(3deg);
-    width: 60px;
-    right: -30px;
-    height: 101%;
-
-    @media (min-width: 900px) {
-      display: initial;
-    }
-  }
 }
-
-.background {
-  display: none;
-  flex: 2;
-  background-size: cover;
-  background-image: url("../assets/background.png");
-  width: 100%;
-  height: 100%;
-
-  @media (min-width: 900px) {
-    display: initial;
-  }
-}
-
-  
-  </style>
+</style>
