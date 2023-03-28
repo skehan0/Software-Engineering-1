f7789fd7ddbf3bce9812ff7a698850352838ad7e
dizzfbma <115659270+dizzfbma@users.noreply.github.com>
2023-03-28 17:16:33 +0100

    changed the nav

--- vue-project/src/components/Navigation.vue
+++ vue-project/src/components/Navigation.vue
@@ -10,10 +10,14 @@
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ms-auto">
           <li class="nav-item mx-0 mx-lg-1"><router-link class="nav-link py-3 px-0 px-lg-3 rounded" to="/blog">Blog</router-link></li>
-          <li class="nav-item mx-0 mx-lg-1"><router-link class="nav-link py-3 px-0 px-lg-3 rounded" to="/register">Register</router-link></li>
-          <li class="nav-item mx-0 mx-lg-1"><router-link class="nav-link py-3 px-0 px-lg-3 rounded" to="/login">Login</router-link></li>
-          <li class="nav-item mx-0 mx-lg-1"><router-link class="nav-link py-3 px-0 px-lg-3 rounded" to="/about">About</router-link></li>
-          <li class="nav-item mx-0 mx-lg-1"><router-link class="nav-link py-3 px-0 px-lg-3 rounded" to="/">Logout</router-link></li>
+          <li v-if="!isLoggedIn" class="nav-item mx-0 mx-lg-1"><router-link class="nav-link py-3 px-0 px-lg-3 rounded" to="/register">Register</router-link></li>
+          <li v-if="!isLoggedIn" class="nav-item mx-0 mx-lg-1"><router-link class="nav-link py-3 px-0 px-lg-3 rounded" to="/login">Login</router-link></li>
+          <li v-if="!isLoggedIn" class="nav-item mx-0 mx-lg-1"><router-link class="nav-link py-3 px-0 px-lg-3 rounded" to="/about">About</router-link></li>
+          <li v-if="isLoggedIn" class="nav-item mx-0 mx-lg-1"><router-link class="nav-link py-3 px-0 px-lg-3 rounded" to="/secure">Secure</router-link></li>
+          <li v-if="isLoggedIn" class="nav-item mx-0 mx-lg-1"><router-link class="nav-link py-3 px-0 px-lg-3 rounded" to="/CreateMenu">CreateMenu</router-link></li>
+          <li v-if="isLoggedIn" class="nav-item mx-0 mx-lg-1"><router-link class="nav-link py-3 px-0 px-lg-3 rounded" to="/blog">Blog</router-link></li>
+          <li v-if="isLoggedIn" class="nav-item mx-0 mx-lg-1"><router-link @click="logout" class="nav-link py-3 px-0 px-lg-3 rounded"
+                                                                           to="/">Logout</router-link></li>
         </ul>
       </div>
     </div>
@@ -22,10 +26,61 @@
 </template>
 
 <script>
-export default {
-  name:  "Navigation",
-  
-};
+import app from "../api/firebase"
+  ;
+import
+{getAuth, onAuthStateChanged, signOut} from "firebase/auth"
+  ;
+import Login from "@/pages/Login.vue";
+export default
+{
+  name: "Navigation"
+  ,
+  components: {Login},
+  data() {
+    return
+    {
+      isLoggedIn : false
+    }
+  }
+  ,
+  created (){
+// Check if the user is logged in
+    const auth = getAuth
+        (app
+        )
+    ;
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console
+            .log(user)
+        ;
+        this
+            .isLoggedIn = true;
+      } else
+      {
+        this
+            .isLoggedIn = false;
+      }
+    })
+    ;
+  }
+  ,
+  methods : {
+    logout(){
+      signOut
+      (getAuth
+      (app)).then(() => {
+// Send them back to the home page!
+        this
+            .$router.push("/"
+        )
+        ;
+      })
+      ;
+    }
+  }
+}
 </script>
 
 <style scoped>
