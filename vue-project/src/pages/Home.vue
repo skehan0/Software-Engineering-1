616d9a8eb7ccea8b3a2a65aae4be65ec8f857ae3
Crashkila101 <concoolmccarthy@gmail.com>
2023-05-03 16:31:33 +0100

    Made changes to homepage

--- vue-project/src/pages/Home.vue
+++ vue-project/src/pages/Home.vue
@@ -1,41 +1,35 @@
 <template>
     <div class="home">
-        <div class="row-1">
+      <div class="row-1">
       <h1>
-        Join The Future With <span class="text-warning"> Digital Transformation </span>
+        Join the Future of Bars and Restaurants<span class="text-warning"> With Spice </span>
       </h1>
       <p class="lead my-4">
-        Digital transformation is the process of using digital technologies to create new — or modify existing — business
-        processes, culture, and customer experiences to meet changing business and market requirements. This reimagining of
-        business in the digital age is digital transformation
+        Spice helps bars and restaurants cut down on extra costs by eliminating the need for waiting tables and
+        ordering at counters, creating an efficient means to deliver the best possible product to the consumer.
       </p>
-    </div>
-    <div class="home-about">
+      </div>
       <div class="row-2">
-        <span>why choose us?</span>
-        <h3 class="title">what's make our app so good?</h3>
+        <span>WHY CHOOSE US?</span>
+        <h3 class="title">what makes spice so good?</h3>
         <p>
-          Customers value the very best from their drinks. The purpose of Spice is to help more pubs and restaurants
-          adapt to modern technology by solving many problems such as avoiding congestion at bars. With our app,
-          you can avoid navigating through a crowded pub carrying a sizable round of drinks on a tray. The app can also
-          be an excellent tool for those who are less mobile or seated far from the bar, such beer gardens or on an upper
-          floor.
+          Customers value the very best from their drinks. The purpose of Spice is to help pubs and restaurants
+          adapt to modern technology by solving many problems such as avoiding congestion at bars. With Spice,
+          you can avoid spilling a tray of drinks while you navigate through a crowded bar. Spice is also an 
+          excellent tool for those who are less mobile, but still like to drink with their friends on the weekends.
+          With spice, you won't ever have to move from your seat again, leaving more time for the important things in life.
         </p>
-        <div class="icons-container">
-          <div class="icons">
-            <h3>fast technology</h3>
-          </div>
-          <div class="icons">
-            <h3>fresh drinks</h3>
-          </div>
-          <div class="icons">
-            <h3>24/7 support</h3>
-          </div>
+        <h3 class="subtitle">HOW DOES IT WORK?</h3>
+        <p>
+          Bars and restaurants register with us and create their menu. After that, users of Spice can locate any
+          business registered with us and create their order by providing their table number and order details. 
+          their order is sent to the POS system in the kitchen or at the barfront, and when the order has been fulfilled,
+          the customer's phone will get a notification to notify them of their order. Alternatively, restaurants can choose to
+          serve the items to the customer.
+        </p>
         </div>
         <router-link @click="scrollToTop()" to="/about" class="btn">read more</router-link>
       </div>
-    </div>
-</div>
   </template>
   
   <script>
@@ -55,6 +49,12 @@
   margin: 90px auto 0;
   text-align: center;
   max-width: 1000px;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  padding: 5% 0;
 }
 
 .home h1 {
@@ -71,27 +71,18 @@
   color: #27ae60;
 }
 
-.home-about {
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
-  background-color: white;
-  padding: 5% 0;
+
+.home .row-2 {
 }
 
-.home-about .row-2 {
-  flex: 1 1 40rem;
-}
-
-.home-about .row-2 span {
-  font-family: "Satisfy", cursive;
+.home .row-2 span {
   font-size: 3rem;
+  font-weight: 500;
   color: #27ae60;
   display: block;
 }
 
-.home-about .row-2 .title {
+.home .row-2 .title {
   font-size: 3rem;
   padding-top: 0.5rem;
   color: #130f40;
@@ -101,7 +92,14 @@
   margin-bottom: 2rem;
 }
 
-.home-about .row-2 p {
+.home .row-2 .subtitle {
+  font-size: 3rem;
+  font-weight: 500;
+  color: #27ae60;
+  display: block;
+}
+
+.home .row-2 p {
   padding: 1rem 0;
   line-height: 2;
   font-size: 1.6rem;
@@ -109,21 +107,7 @@
   margin-bottom: 2rem;
 }
 
-.home-about .row-2 .icons-container .icons {
-  flex: 1 1 20rem;
-  border-radius: 0.5rem;
-  background: #fff;
-  box-shadow: 0 1rem 1rem rgba(0, 0, 0, 0.05);
-  display: flex;
-  align-items: center;
-  gap: 2rem;
-  padding: 2rem;
-}
 
-.home-about .row-2 .icons-container .icons h3 {
-  font-size: 1.7rem;
-  color: #130f40;
-}
 
 @media only screen and (max-width: 768px) {
   .home {
@@ -139,29 +123,29 @@
     line-height: 2rem;
   }
 
-  .home-about .image {
+  .home .image {
     flex: 1 1 100%;
     margin-bottom: 1rem;
   }
 
-  .home-about .row-2 {
+  .home .row-2 {
     flex: 1 1 100%;
   }
 
-  .home-about .row-2 span {
+  .home .row-2 span {
     font-size: 2rem;
   }
 
-  .home-about .row-2 .title {
+  .home .row-2 .title {
     font-size: 2rem;
   }
 
-  .home-about .row-2 p {
+  .home .row-2 p {
     font-size: 1.3rem;
     line-height: 1.8rem;
   }
 
-  .home-about .row-2 .icons-container .icons h3 {
+  .home .row-2 .icons-container .icons h3 {
     font-size: 1.3rem;
   }
 }
